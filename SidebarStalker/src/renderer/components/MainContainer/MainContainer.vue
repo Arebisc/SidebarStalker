926b16dad7f5d57743e81152d76b86f09930230b
Arebis <michal.sibera@windowslive.com>
2018-04-02 21:26:51 +0200

    Added presence table mock

--- SidebarStalker/src/renderer/components/MainContainer/MainContainer.vue
+++ SidebarStalker/src/renderer/components/MainContainer/MainContainer.vue
@@ -1,11 +1,48 @@
 <template>
     <div class="main-container__wrapper">
-        <h1>MainContainer</h1>
+        <el-table :data="tableData" style="width: 100%">
+            <el-table-column
+                prop="firstname"
+                label="Imię">
+            </el-table-column>
+            <el-table-column
+                prop="lastname"
+                label="Nazwisko">
+            </el-table-column>
+            <el-table-column
+                prop="index"
+                label="Indeks">
+            </el-table-column>
+        </el-table>
     </div>
 </template>
 <script>
 export default {
-
+    data() {
+        return {
+            tableData: [{
+                    firstname: 'Michał',
+                    lastname: 'Sibera',
+                    index: '126827'
+                }, 
+                {
+                    firstname: 'Dawid',
+                    lastname: 'Wojciechowski',
+                    index: '126112'
+                }, 
+                {
+                    firstname: 'Sebastian',
+                    lastname: 'Niespodziany',
+                    index: '126222'
+                }, 
+                {
+                    firstname: 'Bartłomiej',
+                    lastname: 'Wierzbinski',
+                    index: '126333'
+                }
+            ]
+        }
+    }
 }
 </script>
 <style lang="scss">
