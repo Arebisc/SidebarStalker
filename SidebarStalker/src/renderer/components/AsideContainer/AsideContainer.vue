2db171552e2b35fb4b87d9f006d83fab574eda1d
Arebis <michal.sibera@windowslive.com>
2018-04-24 19:30:51 +0200

    Fixes after Dawid's layout changes

--- SidebarStalker/src/renderer/components/AsideContainer/AsideContainer.vue
+++ SidebarStalker/src/renderer/components/AsideContainer/AsideContainer.vue
@@ -2,8 +2,6 @@
     <el-menu
       default-active="2"
       class="el-menu-vertical-demo nav-bar"
-      @open="handleOpen"
-      @close="handleClose"
       background-color="#545c64"
       text-color="#fff"
       active-text-color="#ffd04b"
