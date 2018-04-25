cad5ffcc9bea49bff2ed235a85df9264c2c9fdfc
Arebis <michal.sibera@windowslive.com>
2018-04-25 10:15:23 +0200

    Directory path's fixes in Count.js

--- SidebarStalker/src/core/visionDetection/Count.js
+++ SidebarStalker/src/core/visionDetection/Count.js
@@ -1,19 +1,22 @@
 const PythonShell = require('python-shell');
- 
+const baseProjectDirectory = process.cwd();
+
 let options = {
   //mode: 'text',
   //pythonPath: 'path/to/python',
  // pythonOptions: ['-u'],
  // scriptPath: 'path/to/my/scripts',
   args: [
-    '--model=C:\\Projects\\Politechnika\\Semestr VI\\SidebarStalker\\SidebarStalker\\src\\core\\visionDetection\\MobileNetSSD_deploy.caffemodel',
-    '--prototxt=C:\\Projects\\Politechnika\\Semestr VI\\SidebarStalker\\SidebarStalker\\src\\core\\visionDetection\\MobileNetSSD_deploy.prototxt',
+    '--model=' + baseProjectDirectory + '\\src\\core\\visionDetection\\MobileNetSSD_deploy.caffemodel',
+    '--prototxt=' + baseProjectDirectory + '\\src\\core\\visionDetection\\MobileNetSSD_deploy.prototxt',
     '--camera_Port=8080, ',
-    '--camera_IP=192.168.1.13']
+    '--camera_IP=192.168.43.1']
 };
+
+
 export function Detect() {
   return new Promise((resolve, reject) => {
-    let scriptFilename = 'C:\\Projects\\Politechnika\\Semestr VI\\SidebarStalker\\SidebarStalker\\src\\core\\visionDetection\\Detection.py';
+    let scriptFilename = process.cwd() + '\\src\\core\\visionDetection\\Detection.py';
 
     PythonShell.run(scriptFilename, options, function (err, results) {
       if (err)
