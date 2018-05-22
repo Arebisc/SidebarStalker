876b24ced9214670c85464d3b7338fe7cf5e4c71
Arebis <michal.sibera@windowslive.com>
2018-05-22 09:22:10 +0200

    Moved exporting to header

--- SidebarStalker/src/renderer/components/HeaderContainer/HeaderContainer.vue
+++ SidebarStalker/src/renderer/components/HeaderContainer/HeaderContainer.vue
@@ -10,19 +10,78 @@
   <el-menu-item index="1">Tabela studentów</el-menu-item>
   <el-submenu index="2">
     <template slot="title">Eksport</template>
-    <el-menu-item index="2-1">PDF</el-menu-item>
-    <el-menu-item index="2-2" @click="triggerExport">XLS</el-menu-item>
+    <el-menu-item index="2-1">
+        <download-excel
+            class = "btn btn-default"
+            :data = "studentsData"    
+            type="csv"
+            name = "filename.csv"
+            :meta="tableMeta"
+            :param="tableHeaders"
+            :fields = "tableFields"
+            >
+            Exportuj csv
+        </download-excel>
+    </el-menu-item>
+    <el-menu-item index="2-2" @click="triggerExport">
+        <download-excel
+            class = "btn btn-default"
+            :data = "studentsData"    
+            type="xls"
+            name = "filename.xls"
+            :meta="tableMeta"
+            :param="tableHeaders"
+            :fields = "tableFields"
+            >
+            Exportuj xls
+        </download-excel>
+    </el-menu-item>
   </el-submenu>
 </el-menu>
 </template>
 <script>
 import { EventBus } from '@/helpers/eventBusHelpers';
+import JsonExcel from 'vue-json-to-excel/JsonExcel';
+import { mapGetters } from 'vuex';
+
 
 export default {
+    components: {
+      'downloadExcel': JsonExcel
+    },
+
      data() {
       return {
         activeIndex: '1',
-        activeIndex2: '1'
+
+        activeIndex2: '1',
+
+        tableHeaders : {
+            "date": "Data",
+            "firstname": "Imię",
+            "lastname": "Nazwisko",
+            "index": "Index"
+        },
+
+        tableFields: {
+            "Data": "String",
+            "Imię": "String",
+            "Nazwisko": "String",
+            "Index": "String"
+        },
+
+        tableMeta: [
+            [
+                {
+                    'key': 'charset',
+                    'value': 'utf-8'
+                }
+            ]
+        ],
+
+        titles: [
+            'Data i godzina', 'Imię', 'Nazwisko', 'Index'
+        ],
       };
     },
 
@@ -34,7 +93,13 @@ export default {
       triggerExport() {
         EventBus.$emit("exportTableToXls");
       }
-    }
+    },
+
+    computed: {
+        ...mapGetters([ 
+            'studentsData'
+        ])
+    },
 }
 </script>
 <style lang="scss" scoped>
