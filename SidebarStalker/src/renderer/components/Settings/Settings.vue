72cd4cfffa66124dc7da41ac32e94520d0b1cb0c
Arebis <michal.sibera@windowslive.com>
2018-05-26 19:24:07 +0200

    Added settings component

---
+++ SidebarStalker/src/renderer/components/Settings/Settings.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="settings-container__wrapper">
+        
+    </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style lang="scss">
+@import './Settings.scss';
+</style>
