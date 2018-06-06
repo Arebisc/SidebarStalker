6c1e70f97e1fdbe243e75b724fe034d51ea2da74
Arebis <michal.sibera@windowslive.com>
2018-06-06 12:19:32 +0200

    Added functions for reading and writing appsettings.json file

--- SidebarStalker/src/renderer/helpers/sidebarStalkerUtils.js
+++ SidebarStalker/src/renderer/helpers/sidebarStalkerUtils.js
@@ -1,4 +1,10 @@
+import fs from 'fs';
+
+const appsettingsPath = './static/appsettings.json';
+const encoding = "utf8";
+
 export class SidebarStalkerUtils {
+
     static getUserLanguage() {
         let lang = 'pl';
         switch (window.navigator.userLanguage || window.navigator.language) {
@@ -10,5 +16,32 @@ export class SidebarStalkerUtils {
                 break;
         }
         return lang;
+    }
+
+    static readAppsettingsFile = () => {  
+        return new Promise((resolve, reject) => {
+            fs.readFile(appsettingsPath, encoding, (err, data) => {
+                debugger;
+                if(err) {
+                    reject(err);
+                }
+                else {
+                    resolve(JSON.parse(data));
+                }
+            });
+        });
+    }
+
+    static saveAppsettingsFile = (appsettingsData) => {
+        return new Promise((resolve, reject) => {
+            fs.writeFile(appsettingsPath, appsettingsData, encoding, (err) => {
+                if(err) {
+                    reject(err);
+                }
+                else {
+                    resolve(true);
+                }
+            });
+        });
     }
 }
